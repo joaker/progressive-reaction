a7fa84d4c77db0ff508938c70771497668bc5021
joaker <jooakley@gmail.com>
2016-12-16 23:09:48 -0500

    add jpg as a file extension

--- src/worker/cache/index.js
+++ src/worker/cache/index.js
@@ -1,29 +1,32 @@
 import log from "../log";
 import config from "../config";
-// import {getEventError, getServerErrorResponse} from "../error";
 
+// save the request with the discovered response in the cache
 export const saves = (request, response) => {
   const savesRequest = opensCache().then(function(cache) {
     return cache.put(request, response.clone());
-  }).then(() => response , err => {
-    const errorMessage = 'problem saving request cache';
-    const error = err || new Error(errorMessage);
-    log('problem saving request cache', error);
-    log(`offending response: <${response}>`);
-    // throw error; // don't throw, failing to cache is not a cardinal sin
-    return response;
-  });
-
+  }).then(() => response , handleCacheSaveError(request, response));
   return savesRequest;
 };
 
+// fetch a cached item
 export const gets = (event) => {
   const {request} = event;
   const {url} = request;
-  return opensCache().then(cache => cache.match(event.request));
+  return opensCache().then(cache => cache.match(request));
 }
 
+// Is this a resource we want to cache?
+export const isCacheable = (event) => {
+  console.log(`isCacheable Requested for event: ${JSON.stringify(event)}`)
+  const metadata = createEventMetadata(event);
+  const cacheable = metadata.isCacheable;
+  return cacheable;
+}
+
+
 const opensCache = () => caches.open(config.name);
+
 const getsCacheKeys = () => opensCache().then(cache => {
   return cache.keys().then(keys => {
     return {
@@ -32,6 +35,7 @@ const getsCacheKeys = () => opensCache().then(cache => {
     };
   });
 });
+
 const deletesKeys = () => getsCacheKeys().then(({cache, keys}) => {
     const deletesKey = key => cache.delete(key);
     const deletions = keys.map(deletesKey);
@@ -39,31 +43,28 @@ const deletesKeys = () => getsCacheKeys().then(({cache, keys}) => {
     return allKeysDeleted;
   });
 
-export const clearCacheKeys = () => {
-  return deletesCacheKeys();
-}
-
-const buildPrecache = (filesToCache=[]) => (cache) => {
-  log(`files; ${filesToCache.join(',')}`)
-  for (let publicPath of filesToCache) {
-    log("public path: " + publicPath);
-  }
-  return cache.addAll(filesToCache);
-};
+const createPrecacher = (filesToCache=[]) => (cache) => cache.addAll(filesToCache);
 
 export const precaches = () => {
   const precacheFiles = config.staticFiles;
-  const cacheFiles = buildPrecache(precacheFiles);
-  log('openging cache');
-  return caches.open(config.name).then(cacheFiles);
+  const precaches = createPrecacher(precacheFiles);
+  return caches.open(config.name).then(precaches);
 };
 
 
 const defaultMetadata = {};
+const cacheableFileTypes = [
+  'js',
+  'jpg',
+  'jpeg',
+  'css',
+  'jpg',
+  'htm.*',
+];
+
+const cacheableFilePattern = `\\.(${cacheableFileTypes.join('|')})$`;
+
 const createEventMetadata = (event) => {
-
-  log(`event is: ${JSON.stringify(event)}`)
-
   const {request = {}} = event;
   const {url = ""} = request;
   if(!url) return defaultMetadata;
@@ -77,26 +78,19 @@ const createEventMetadata = (event) => {
   }
 
   // request type definitions
-  const isScript= /\.js$/.test(url);
-  const isStyle= /\.css$/.test(url);
-  const isHTML= /\.htm.*/.test(url);
+  const isCacheableFile = url.match(cacheableFilePattern);
   const isPath = /\/$/.test(url);
 
   // list of request types that are cacheable
   const cacheableCriteria = [
-    isScript,
-    isStyle,
-    isHTML,
-    isPath,
+    isCacheableFile,
     isPath,
   ];
 
   const cacheable = cacheableCriteria.some(cc => cc);
 
   const metadata = {
-    isScript,
-    isStyle,
-    isHTML,
+    isCacheableFile,
     isPath,
     isCacheable: cacheable,// Does this request match any of the criteria?
   };
@@ -104,14 +98,14 @@ const createEventMetadata = (event) => {
   return metadata;
 };
 
-// Is this a resource we want to cache?
-export const isCacheable = (event) => {
-  console.log(`isCacheable Requested for event: ${JSON.stringify(event)}`)
-  const metadata = createEventMetadata(event);
-  const cacheable = metadata.isCacheable;
-  return cacheable;
-}
 
+const handleCacheSaveError = (request, response) => err => {
+  const error = err || new Error('Unknown cache save error');
+  log('problem saving request cache:', error);
+  log(`offending response: <${response}>`);
+  // don't throw an exception - failing to cache is not a cardinal sin
+  return response;
+};
 
 
 export default {
